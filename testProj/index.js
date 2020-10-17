8a00c04201ad42df83c29bc11a1c7d5cc41c72e8
singhaz25 <amarinder_pal@hotmail.com>
2020-10-17 15:25:59 +0530

    Update node message to version 2

--- testProj/index.js
+++ testProj/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-	res.send("Udacity Final project by Amarinder");
+	res.send("Udacity Final project by Amarinder v2 ");
 });
 
 app.get('/about', function(req, res){
