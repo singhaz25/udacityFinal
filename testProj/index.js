e9373ee638bfe23745df6de0fdd043053ecae006
singhaz25 <amarinder_pal@hotmail.com>
2020-10-17 11:52:39 +0530

    Added about API end point to node application

--- testProj/index.js
+++ testProj/index.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-	res.send("Hello world! version 1");
+	res.send("Udacity Final project by Amarinder");
+});
+
+app.get('/about', function(req, res){
+	res.send("Udacity Final Jenkins Build @BuildNumber@ / @BuildDate@");
 });
 
 app.listen(3000);
